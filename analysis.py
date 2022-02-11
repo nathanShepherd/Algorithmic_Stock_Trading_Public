8420ba65ab2976ec3f0b199cf20a28b05fdbc00c
nateshep <nateshep@localhost.localdomain>
2022-02-11 03:05:43 -0500

    Linux Backup

--- analysis.py
+++ analysis.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn import preprocessing
 
 
 np.random.seed(406)
@@ -17,4 +20,34 @@ def random_split():
     
     import pdb; pdb.set_trace()
 
-random_split()
+def calc_normal_returns():
+    np.random.seed(406)
+    df = pd.read_csv('joined_df_stats/sp500_joined_Closed_prices.csv')
+
+    df.plot()
+    plt.show()
+    plt.savefig("joined_cl.png")
+
+    dates = df.iloc[:, 0]
+    values = df.iloc[:,1:].interpolate(axis="columns").values
+    values_trim = values[:-1]
+    #import pdb; pdb.set_trace()
+
+    #dates, values = values[]
+    '''
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(values)
+
+    x_agg_mean = np.mean(x_scaled, axis=1)
+
+    randnorm_prices = np.random.normal(0.005,.1, size=x_scaled.shape)
+    norm_agg_mean = np.mean(randnorm_prices, axis=1)
+    '''
+    compile_df = pd.DataFrame({"mean_sp": x_agg_mean,
+                               #"mode": row_mode_arr,
+                               "rand_norm": norm_agg_mean,
+                              },
+                               index=dates)
+                            
+
+calc_normal_returns()
